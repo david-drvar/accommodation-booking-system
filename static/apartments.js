3601b76045e6ab3317c78ab7856e19d6502af57f
Ilija Brdar <ilija_brdar@yahoo.com>
2020-08-28 19:15:27 +0200

    add new apartment - google location

--- static/apartments.js
+++ static/apartments.js
@@ -69,114 +69,185 @@ Vue.component("apartments", {
         }
     },
     template : `
-    <div>
-        <button class="btn btn-outline-primary" data-toggle="modal" data-target="#newApartment">New</button>
-        <div class="modal fade" id="newApartment" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">New apartment
-                <span class="text-secondary" v-if="page==0">GENERAL</span> 
-                <span class="text-secondary" v-if="page==1">INVENTORY</span></h5> 
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div class="modal-body">  
-                <br>
-                <div v-if="page==0">
-                      <div class="form-row">
-                        <div class="col-md-12">
-                          <select class="form-control" v-model="apartment.type">
-                            <option selected disabled>Type</option>
-                            <option>ROOM</option>
-                            <option>FULL</option>
-                          </select>
-                        </div>
-                      </div>
-                      <br/>
-                      <div class="form-row">
-                        <div class="col-md-6">
-                          <input type="number" class="form-control" placeholder="number of rooms" min="0"
-                          v-model="apartment.roomNumber">
-                        </div>
-                        <div class="col-md-6">
-                          <input type="number" class="form-control" placeholder="number of guests" min="0"
-                          v-model="apartment.guestNumber">
-                        </div>
-                      </div>
-                      <br/>
-                      <div class="form-row">
-                        <div class="col-md-6">
-                          <select class="form-control">
-                            <option selected disabled>State</option>
-                            <option>...</option>
-                          </select>
-                        </div>
-                        <div class="col-md-6">
-                          <select class="form-control">
-                            <option selected disabled>Town</option>
-                            <option>...</option>
-                          </select>
-                        </div>
-                      </div>
-                      <br/>
-                      <div class="form-row">
-                        <div class="col-md-12">
-                          <input type="text" class="form-control" placeholder="Street">
-                        </div>
-                      </div>
-                      <br/>
-                      <div class="form-row">
-                        <div class="input-group col-md-12">
-                          <div class="input-group-prepend">
-                            <span class="input-group-text">$</span>
-                           </div>
-                          <input type="number" class="form-control" placeholder="Price per night" min="0"
-                          v-model="apartment.pricePerNight">
-                          <div class="input-group-append">
-                            <span class="input-group-text">.00</span>
-                          </div>
-                        </div>
-                      </div>
-                      <br/>
-                      <div class="form-row">
-                        <div class="col-md-6">
-                          <input type="time" class="form-control" placeholder="Time to check in"
-                          v-model="apartment.checkIn">
-                        </div>
-                        <div class="col-md-6">
-                          <input type="time" class="form-control" placeholder="Time to check out"
-                          v-model="apartment.checkOut">
-                        </div>
-                      </div>
-                </div>
-                <div v-if="page==1">
-                    <div>
-                        <button class="btn btn-outline-secondary col-md-4" 
-                        data-toggle="button" aria-pressed="false"
-                        v-for="a in amenities"
-                        v-on:click="addAmenity($event, a)">
-                            {{a.name}}
-                        </button>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6">
+                <div class="form-row">
+                    <div class="col-md-12">
+                      <select class="form-control mt-4" v-model="apartment.type">
+                        <option selected disabled>Type</option>
+                        <option>ROOM</option>
+                        <option>FULL</option>
+                      </select>
                     </div>
-                    <br/>
-                    <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="inputGroupFile01" multiple>
-                        <label class="custom-file-label" for="inputGroupFile01">Drag & Drop images here</label>
+                  </div>
+                  <br/>
+                  <div class="form-row">
+                    <div class="col-md-6">
+                      <input type="number" class="form-control" placeholder="number of rooms" min="0"
+                      v-model="apartment.roomNumber">
                     </div>
+                    <div class="col-md-6">
+                      <input type="number" class="form-control" placeholder="number of guests" min="0"
+                      v-model="apartment.guestNumber">
+                    </div>
+                  </div>
+                  <br/>
+                  <div class="form-row">
+                    <div class="col-md-8">
+                        <input
+                          id="pac-input"
+                          class="form-control"
+                          type="text"
+                          placeholder="Location"
+                        />
+                    </div>
+                    <div class="col-md-4">
+                        <input 
+                          class="form-control"
+                          type="text"
+                          placeholder="Postal number"
+                        />
+                    </div>
+                  </div>
+                  <br/>
+                  <div class="form-row">
+                    <div class="input-group col-md-12">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text">$</span>
+                       </div>
+                      <input type="number" class="form-control" placeholder="Price per night" min="0"
+                      v-model="apartment.pricePerNight">
+                      <div class="input-group-append">
+                        <span class="input-group-text">.00</span>
+                      </div>
+                    </div>
+                  </div>
+                  <br/>
+                  <div class="form-row">
+                    <div class="col-md-6">
+                      <input type="time" class="form-control" placeholder="Time to check in"
+                      v-model="apartment.checkIn">
+                    </div>
+                    <div class="col-md-6">
+                      <input type="time" class="form-control" placeholder="Time to check out"
+                      v-model="apartment.checkOut">
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-primary" v-if="page == 1" v-on:click="previousPage">Back</button>
-                <button type="button" class="btn btn-primary" v-if="page == 0" v-on:click="nextPage">Next</button>
-                <button type="button" class="btn btn-primary" v-if="page == 1"
-                v-on:click="saveApartment">Save</button>
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-              </div>
+                <div class="col-lg-6">
+                        <div id="map"></div>
+                </div>
             </div>
-          </div>
         </div>
-    </div>    
+<!--    <div>-->
+<!--        <button class="btn btn-outline-primary" data-toggle="modal" data-target="#newApartment">New</button>-->
+<!--        <div class="modal fade" id="newApartment" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">-->
+<!--          <div class="modal-dialog modal-dialog-centered" role="document">-->
+<!--            <div class="modal-content">-->
+<!--              <div class="modal-header">-->
+<!--                <h5 class="modal-title" id="exampleModalLabel">New apartment-->
+<!--                <span class="text-secondary" v-if="page==0">GENERAL</span> -->
+<!--                <span class="text-secondary" v-if="page==1">INVENTORY</span></h5> -->
+<!--                <button type="button" class="close" data-dismiss="modal" aria-label="Close">-->
+<!--                  <span aria-hidden="true">&times;</span>-->
+<!--                </button>-->
+<!--              </div>-->
+<!--              <div class="modal-body">  -->
+<!--                <br>-->
+<!--                <div v-if="page==0">-->
+<!--                      <div class="form-row">-->
+<!--                        <div class="col-md-12">-->
+<!--                          <select class="form-control" v-model="apartment.type">-->
+<!--                            <option selected disabled>Type</option>-->
+<!--                            <option>ROOM</option>-->
+<!--                            <option>FULL</option>-->
+<!--                          </select>-->
+<!--                        </div>-->
+<!--                      </div>-->
+<!--                      <br/>-->
+<!--                      <div class="form-row">-->
+<!--                        <div class="col-md-6">-->
+<!--                          <input type="number" class="form-control" placeholder="number of rooms" min="0"-->
+<!--                          v-model="apartment.roomNumber">-->
+<!--                        </div>-->
+<!--                        <div class="col-md-6">-->
+<!--                          <input type="number" class="form-control" placeholder="number of guests" min="0"-->
+<!--                          v-model="apartment.guestNumber">-->
+<!--                        </div>-->
+<!--                      </div>-->
+<!--                      <br/>-->
+<!--                      <div class="form-row">-->
+<!--                        <div class="col-md-6">-->
+<!--                          <select class="form-control">-->
+<!--                            <option selected disabled>State</option>-->
+<!--                            <option>...</option>-->
+<!--                          </select>-->
+<!--                        </div>-->
+<!--                        <div class="col-md-6">-->
+<!--                          <select class="form-control">-->
+<!--                            <option selected disabled>Town</option>-->
+<!--                            <option>...</option>-->
+<!--                          </select>-->
+<!--                        </div>-->
+<!--                      </div>-->
+<!--                      <br/>-->
+<!--                      <div class="form-row">-->
+<!--                        <div class="col-md-12">-->
+<!--                          <input type="text" class="form-control" placeholder="Street">-->
+<!--                        </div>-->
+<!--                      </div>-->
+<!--                      <br/>-->
+<!--                      <div class="form-row">-->
+<!--                        <div class="input-group col-md-12">-->
+<!--                          <div class="input-group-prepend">-->
+<!--                            <span class="input-group-text">$</span>-->
+<!--                           </div>-->
+<!--                          <input type="number" class="form-control" placeholder="Price per night" min="0"-->
+<!--                          v-model="apartment.pricePerNight">-->
+<!--                          <div class="input-group-append">-->
+<!--                            <span class="input-group-text">.00</span>-->
+<!--                          </div>-->
+<!--                        </div>-->
+<!--                      </div>-->
+<!--                      <br/>-->
+<!--                      <div class="form-row">-->
+<!--                        <div class="col-md-6">-->
+<!--                          <input type="time" class="form-control" placeholder="Time to check in"-->
+<!--                          v-model="apartment.checkIn">-->
+<!--                        </div>-->
+<!--                        <div class="col-md-6">-->
+<!--                          <input type="time" class="form-control" placeholder="Time to check out"-->
+<!--                          v-model="apartment.checkOut">-->
+<!--                        </div>-->
+<!--                      </div>-->
+<!--                </div>-->
+<!--                <div v-if="page==1">-->
+<!--                    <div>-->
+<!--                        <button class="btn btn-outline-secondary col-md-4" -->
+<!--                        data-toggle="button" aria-pressed="false"-->
+<!--                        v-for="a in amenities"-->
+<!--                        v-on:click="addAmenity($event, a)">-->
+<!--                            {{a.name}}-->
+<!--                        </button>-->
+<!--                    </div>-->
+<!--                    <br/>-->
+<!--                    <div class="custom-file">-->
+<!--                        <input type="file" class="custom-file-input" id="inputGroupFile01" multiple>-->
+<!--                        <label class="custom-file-label" for="inputGroupFile01">Drag & Drop images here</label>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--              </div>-->
+<!--              <div class="modal-footer">-->
+<!--                <button type="button" class="btn btn-primary" v-if="page == 1" v-on:click="previousPage">Back</button>-->
+<!--                <button type="button" class="btn btn-primary" v-if="page == 0" v-on:click="nextPage">Next</button>-->
+<!--                <button type="button" class="btn btn-primary" v-if="page == 1"-->
+<!--                v-on:click="saveApartment">Save</button>-->
+<!--                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>-->
+<!--              </div>-->
+<!--            </div>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--    </div>    -->
     `
 })
--- static/map.js
+++ static/map.js
@@ -4,65 +4,167 @@
 // This example requires the Places library. Include the libraries=places
 // parameter when you first load the API. For example:
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-function initAutocomplete() {
+// function initAutocomplete() {
+//     const map = new google.maps.Map(document.getElementById("map"), {
+//         center: { lat: -33.8688, lng: 151.2195 },
+//         zoom: 13,
+//         mapTypeId: "roadmap"
+//     });
+//
+//     const componentForm = {
+//         street_number: "short_name",
+//         route: "long_name",
+//         locality: "long_name",
+//         administrative_area_level_1: "short_name",
+//         country: "long_name",
+//         postal_code: "short_name"
+//     };
+//
+//
+//
+//     // Create the search box and link it to the UI element.
+//     const input = document.getElementById("pac-input");
+//     const searchBox = new google.maps.places.SearchBox(input);
+//
+//     //map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+//     // Bias the SearchBox results towards current map's viewport.
+//     map.addListener("bounds_changed", () => {
+//         searchBox.setBounds(map.getBounds());
+//     });
+//     let markers = [];
+//     // Listen for the event fired when the user selects a prediction and retrieve
+//     // more details for that place.
+//     searchBox.addListener("places_changed", () => {
+//         let placeInfo = {};
+//         const places = searchBox.getPlaces();
+//
+//         if (places.length == 0) {
+//             return;
+//         }
+//         // Clear out the old markers.
+//         markers.forEach(marker => {
+//             marker.setMap(null);
+//         });
+//         markers = [];
+//         // For each place, get the icon, name and location.
+//         const bounds = new google.maps.LatLngBounds();
+//         places.forEach(place => {
+//             if (!place.geometry) {
+//                 console.log("Returned place contains no geometry");
+//                 return;
+//             }
+//             const icon = {
+//                 url: place.icon,
+//                 size: new google.maps.Size(71, 71),
+//                 origin: new google.maps.Point(0, 0),
+//                 anchor: new google.maps.Point(17, 34),
+//                 scaledSize: new google.maps.Size(25, 25)
+//             };
+//             console.log(place);
+//             console.log(place.address_components);
+//             for (const component of place.address_components) {
+//                 const addressType = component.types[0];
+//
+//                 if (componentForm[addressType]) {
+//                     const val = component[componentForm[addressType]];
+//                     placeInfo[addressType] = val;
+//                 }
+//             }
+//
+//             console.log(placeInfo);
+//             // Create a marker for each place.
+//             markers.push(
+//                 new google.maps.Marker({
+//                     map,
+//                     icon,
+//                     title: place.name,
+//                     position: place.geometry.location
+//                 })
+//             );
+//
+//             if (place.geometry.viewport) {
+//                 // Only geocodes have viewport.
+//                 bounds.union(place.geometry.viewport);
+//             } else {
+//                 bounds.extend(place.geometry.location);
+//             }
+//         });
+//         map.fitBounds(bounds);
+//     });
+// }
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: -33.8688, lng: 151.2195 },
-        zoom: 13,
-        mapTypeId: "roadmap"
+        zoom: 13
     });
-    // Create the search box and link it to the UI element.
     const input = document.getElementById("pac-input");
-    const searchBox = new google.maps.places.SearchBox(input);
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-    // Bias the SearchBox results towards current map's viewport.
-    map.addListener("bounds_changed", () => {
-        searchBox.setBounds(map.getBounds());
+    //map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
+    const autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.setTypes(['geocode']);
+    // Bind the map's bounds (viewport) property to the autocomplete object,
+    // so that the autocomplete requests use the current map bounds for the
+    // bounds option in the request.
+    autocomplete.bindTo("bounds", map);
+    // Set the data fields to return when the user selects a place.
+    autocomplete.setFields(["address_component", "geometry", "icon", "name"]);
+    const marker = new google.maps.Marker({
+        map,
+        anchorPoint: new google.maps.Point(0, -29)
     });
-    let markers = [];
-    // Listen for the event fired when the user selects a prediction and retrieve
-    // more details for that place.
-    searchBox.addListener("places_changed", () => {
-        const places = searchBox.getPlaces();
 
-        if (places.length == 0) {
+    let placeComponents = {
+        street_number : 'number',
+        route : 'street',
+        locality : 'town',
+        country : 'state',
+        postalCode : 0
+    };
+
+    autocomplete.addListener("place_changed", () => {
+        marker.setVisible(false);
+        const place = autocomplete.getPlace();
+        let placeInfo = {
+            number : 0,
+            street : '',
+            town : '',
+            state : '',
+            lat : 0.0,
+            lng : 0.0
+        };
+        let addressComponents = place.address_components;
+        let geometry = place.geometry;
+
+        console.log(place);
+
+        for(let component of addressComponents) {
+            const addressType = component.types[0];
+            if(placeComponents[addressType])
+                placeInfo[placeComponents[addressType]] = component.long_name;
+        }
+
+        placeInfo.lat = geometry.location.lat();
+        placeInfo.lng = geometry.location.lng();
+
+        localStorage.setItem('googleSearch', JSON.stringify(placeInfo));
+
+        if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
             return;
         }
-        // Clear out the old markers.
-        markers.forEach(marker => {
-            marker.setMap(null);
-        });
-        markers = [];
-        // For each place, get the icon, name and location.
-        const bounds = new google.maps.LatLngBounds();
-        places.forEach(place => {
-            if (!place.geometry) {
-                console.log("Returned place contains no geometry");
-                return;
-            }
-            const icon = {
-                url: place.icon,
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(25, 25)
-            };
-            // Create a marker for each place.
-            markers.push(
-                new google.maps.Marker({
-                    map,
-                    icon,
-                    title: place.name,
-                    position: place.geometry.location
-                })
-            );
 
-            if (place.geometry.viewport) {
-                // Only geocodes have viewport.
-                bounds.union(place.geometry.viewport);
-            } else {
-                bounds.extend(place.geometry.location);
-            }
-        });
-        map.fitBounds(bounds);
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17); // Why 17? Because it looks good.
+        }
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
     });
 }
